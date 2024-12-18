f130880994e2a12021dd8ceab91aba5bb9542c13
finpros-repo <long.nhai@finpros.vn>
2024-12-18 09:26:49 +0700

    them cac ma khac, update ham on_message chi gui message vao kafka 1 phut 1 lan

--- src/extract/source_websocket.py
+++ src/extract/source_websocket.py
@@ -1,23 +1,53 @@
 
 import sys
-import _env
 import json
+import time
+import datetime
 import websocket
-sys.path.append(_env.SYS_PATH_APPEND)
+sys.path.append("../src")
 
+import config
+
+from utils import utils
 from load import producer
+from transform import transform
 
 
 def on_message(ws, message):
-    data = json.loads(message)
-    if type(data) == dict:
-        if data["e"] == "aggTrade":
-            # print(message)
-            producer_aggtrade.send_message(message)
-        elif data["e"] == "kline":
-            producer_candlestick.send_message(message)
-    elif type(data) == list:
-        producer_price.send_message(message)
+    '''
+    Hàm handler tốc độ gửi message vào kafka (vì binance gửi message cho mỗi symbol 0,25s một lần)
+    -> hạn chế lại lượng message: mỗi phút, mỗi symbol được phép gửi vào kafka 1 message
+    - sau khi tất cả các symbol đều được producer gửi message vào kafka, sleep cho tới phút tiếp theo 
+    '''
+    message_dict = json.loads(message)
+    global numb_get_mess
+    global time_old
+    
+    if datetime.datetime.now() > datetime.datetime.now().replace(second=55):
+        # print(datetime.datetime.now())
+        if utils.check_send_mes(numb_get_mess, message_dict):
+            # print("message_raw: ", message_dict)
+            filtered_message = transform.filter_message_candle(message_dict)
+            print("message_filtered", filtered_message)
+            producer_candle.send_message(filtered_message)
+            numb_get_mess[message_dict["s"]]["numb"] += 1
+            numb_get_mess[message_dict["s"]]["last_time"] = int(message_dict["k"]["t"])
+            # print("time_now: ", datetime.datetime.now())
+            print("numb_get_mess sau khi gui kafka: ", numb_get_mess)
+        else:
+            status_check_all_symbol = utils.handler_sleep(numb_get_mess, config.LIST_SYMBOL)
+            # print(status_check_all_symbol)
+            if status_check_all_symbol and ((datetime.datetime.now() - time_old).seconds > 60):
+                for key in numb_get_mess:
+                    numb_get_mess[key]["numb"] = 0
+                time_old = datetime.datetime.now().replace(second=55, microsecond=0)
+                print("time_now: ", datetime.datetime.now())
+                print("numb_get_mess sau khi gui het vao kafka va sang phut moi: ", numb_get_mess)
+            else:
+                # print("passsssss")
+                pass
+    else:
+        pass
 
 def on_error(ws, error):
     print(error)
@@ -30,32 +60,26 @@ def on_open(ws):
     ws.send(json.dumps({
         "method": "SUBSCRIBE",
         "params": [
-            "!ticker@arr"            # All Market Tickers Streams
-            # "btcusdt@aggTrade",      # Aggregate Trade Streams cho BTCUSDT
-            # "ethusdt@aggTrade",      # Aggregate Trade Streams cho ETHUSDT
-            # "btcusdt@kline_1m",      # Kline/Candlestick Streams cho BTCUSDT với interval là 1 phút
-            # "ethusdt@kline_5m"       # Kline/Candlestick Streams cho ETHUSDT với interval là 5 phút
+            "btcusdt@kline_1m",      # Kline/Candlestick Streams cho BTCUSDT với interval là 1 phút python extract/source_websocket.py
+            "ethusdt@kline_1m",
+            "adausdt@kline_1m",
+            "bnbusdt@kline_1m",
+            "solusdt@kline_1m",
+            "xrpusdt@kline_1m",
+            "dogeusdt@kline_1m",
+            "bchusdt@kline_1m"
         ],
         "id": 1
     }))
 
-    ws.send(json.dumps({
-        "method": "SUBSCRIBE",
-        "params": [
-            # "!ticker@arr"            # All Market Tickers Streams
-            "btcusdt@aggTrade",      # Aggregate Trade Streams cho BTCUSDT
-            "ethusdt@aggTrade",      # Aggregate Trade Streams cho ETHUSDT
-            "btcusdt@kline_1m",      # Kline/Candlestick Streams cho BTCUSDT với interval là 1 phút
-            "ethusdt@kline_5m"       # Kline/Candlestick Streams cho ETHUSDT với interval là 5 phút
-        ],
-        "id": 2
-    }))
-
 if __name__ == "__main__":
-    global producer_price, producer_aggtrade, producer_candlestick
-    producer_price = producer.kafkaProducer(_env.TOPIC_CRYPTO_PRICE)
-    producer_aggtrade = producer.kafkaProducer(_env.TOPIC_AGGTRADE)
-    producer_candlestick = producer.kafkaProducer(_env.TOPIC_CANDLESTICK)
+    numb_get_mess = config.NUMB_GET_MESS
+    time_old = datetime.datetime.now().replace(second=55, microsecond=0)
+    timestamp_ms = int(datetime.datetime.now().replace(second=00, microsecond=00).timestamp() * 1000)
+    for key in numb_get_mess:
+        numb_get_mess[key]["last_time"] = timestamp_ms
+
+    producer_candle = producer.kafkaProducer(config.TOPIC_CANDLE)
 
 
     websocket_url = "wss://fstream.binance.com/ws"
